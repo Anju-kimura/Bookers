6a194a33445eafc28f7ffa3d6911944d571d2b72
Anju-kimura <172464703+Anju-kimura@users.noreply.github.com>
2025-08-29 18:16:23 +0000

    [Add]show画面に投稿成功のフラッシュメッセージ,title・body欄とedit・backへの遷移を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -16,7 +17,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/'
+    flash[:notice] = "Book was successfully created."
+    redirect_to "/books/#{@book.id}"
+    #byebug
   end
 
   private
