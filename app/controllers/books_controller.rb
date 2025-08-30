2c7d1c6dde4f58868a22afd90f3138ae4f90e24a
Anju-kimura <172464703+Anju-kimura@users.noreply.github.com>
2025-08-30 07:47:19 +0000

    [Finish?]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -12,14 +13,31 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
-    @book.save
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
     flash[:notice] = "Book was successfully created."
-    redirect_to "/books/#{@book.id}"
-    #byebug
+    redirect_to "/books/#{book.id}"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
